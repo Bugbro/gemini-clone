243733f9ee5efa4b631ee303fca3efbd7e2c64f0
Prabbhat Pal <ppal6060@gmail.com>
2025-08-12 22:05:56 +0530

    Set

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import { GoogleGenAI } from "@google/genai";
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 
-const ai = new GoogleGenAI({apiKey :key_here});
+const ai = new GoogleGenAI({apiKey :"AIzaSyA0618-um1sC6up3oKrdKnhOEXB3xVh9WY"});
 
 async function main(props) {
   const response = await ai.models.generateContent({
